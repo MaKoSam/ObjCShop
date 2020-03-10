70e7a1427b65405722a014cc4669853e98956e3e
MaKoSam <mazniker@icloud.com>
2020-03-10 21:23:28 +0800

    Interface Builder init. Deleted all links to Storybord. Deleted iOS 13 Scenes. Added support for UIWindow. Created RootViewController class for initial controller.

--- AviaTickets/AviaTickets/RootViewController/RootViewController.h
+++ AviaTickets/AviaTickets/RootViewController/RootViewController.h
@@ -1,15 +1,13 @@
 //
-//  ViewController.h
+//  RootViewController.h
 //  AviaTickets
 //
 //  Created by Sam Mazniker on 10/03/2020.
 //  Copyright © 2020 Sam Mazniker. All rights reserved.
 //
-
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface RootViewController : UIViewController
 
 
 @end
-
---
+++ AviaTickets/AviaTickets/RootViewController/RootViewController.m
@@ -0,0 +1,33 @@
+//
+//  RootViewController.m
+//  AviaTickets
+//
+//  Created by Sam Mazniker on 10/03/2020.
+//  Copyright © 2020 Sam Mazniker. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+@interface RootViewController ()
+
+@end
+
+@implementation RootViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+//    self.view.backgroundColor = [UIColor redColor];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
